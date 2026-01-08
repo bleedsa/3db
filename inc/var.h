7e7bd47992390a00518e244a5a23a4d5300583e3
Skylar Bleed <sbleed@proton.me>
2026-01-08 16:46:59 -0600

    fix var.cc mem err

--- inc/var.h
+++ inc/var.h
@@ -8,15 +8,7 @@
 typedef char var_t __attribute__((vector_size(16))); 
 #define VARZ (Z(var_t))
 
-inl auto str_to_var(const char *y) -> var_t {
-	auto ptr = mk<char>(VARZ);
-	auto L = std::min(VARZ, strlen(y)+1);
-	memcpy(ptr, y, L);
-	memset(ptr+L, 0, VARZ-L);
-	ptr[L] = 0;
-	return *(var_t*)ptr;
-}
-
+var_t str_to_var(const char *y);
 char *var_to_str(var_t y);
 
 #endif
--- lib/var.cc
+++ lib/var.cc
@@ -1,4 +1,20 @@
+#include <cassert>
+
 #include <var.h>
+
+auto str_to_var(const char *y) -> var_t {
+	auto len = strlen(y);
+	assert(len > 0);
+
+	auto ptr = mk<char>(VARZ);
+	auto L = std::min(VARZ, len+1);
+
+	memcpy(ptr, y, L);
+	memset(ptr+L, 0, VARZ-L);
+	ptr[L - 1] = 0;
+
+	return *(var_t*)ptr;
+}
 
 auto var_to_str(var_t y) -> char* {
 	auto x = mk<char>(Z(var_t));
