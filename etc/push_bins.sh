a7a10ad88e7c79e1fa2d00ec4bc3523ea4cd9f42
skylar bleed <u0_a210@localhost.(none)>
2026-01-18 22:28:02 -0600

    push_bins.sh

--- etc/push_bins.sh
+++ etc/push_bins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 make -j6 all
-scp o/3db/3db.com badboy.institute:~skye/
-scp o/3i/3i.com   badboy.institute:~skye/
+scp o/3db/3db.com skye@badboy.institute:~skye/
+scp o/3i/3i.com   skye@badboy.institute:~skye/
