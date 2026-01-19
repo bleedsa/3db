57510eb154be0fda933f40a7d6bd794af80be502
Skylar Bleed <sbleed@proton.me>
2026-01-18 22:24:49 -0600

    clean

--- inc/mem.h
+++ inc/mem.h
@@ -6,7 +6,6 @@
 /* make a buffer of X with length L */
 template<typename X>
 inl auto mk(S L) -> X* {
-	std::cout << Z(X)*L << std::endl;
 	auto P = malloc(Z(X)*L);
 	if (!P) {
 		/* NOTE: don't use fatal() for compile time reasons */
