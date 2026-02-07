facf88e7cab13ee8e01c3157692c49161b795bd5
Skylar Bleed <sbleed@proton.me>
2026-02-07 16:15:05 -0600

    fix heavy_insert test

--- run/heavy_insert.cc
+++ run/heavy_insert.cc
@@ -3,19 +3,16 @@
 #include <db.h>
 
 int main() {
-	char *n;
-
 	Three::init();
 
 	for (S i = 0; i < 2222; i++) {
-		asprintf(&n, ".%zu", i);
+		auto n = (std::stringstream() << i).str().c_str();
 		Db::push_ent(n, i);
-		free(n);
 	}
 
 	for (auto &e : Db::ents) {
 		auto str = var_to_str(e.name);
-		std::cout << str << ' ' << e.z;
+		std::cout << str << ' ' << e.z << ' ';
 		delete[] str;
 	}
 
